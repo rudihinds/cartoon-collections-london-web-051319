650a57146f2d1b20900bfaf493791a956fff7b76
rudi hinds <rudihinds@gmail.com>
2019-05-02 22:27:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,9 +19,8 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each do |food| if cheese_types.include?(food)
   return food
-else
-  return nil
   end
+else return nil
 end
 
 end
