92771e077656d3e7dbe0266f2ce04513f62f63e0
rudi hinds <rudihinds@gmail.com>
2019-05-02 22:06:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  number = 1
+  dwarves.each do |dwarf|
+  puts "#{number} #{dwarf}"
+  number += 1
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(powers)
+  powers.map! { |power| power.capitalize }
+  powers.collect { |power| "#{power}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  if array & cheese_types
+    return true
+  else
+    return nil
+  end
 end
+  
+  // take each food from array and pass through cheese_types
+  // if any match return type using find
+  // foodtypes.find {|food| }
+cheese_types = ["cheddar", "gouda", "camembert"]
+end
+
