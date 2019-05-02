aa14fb4f8c8cd31ed2309e70790920bd31c5c37a
rudi hinds <rudihinds@gmail.com>
2019-05-02 22:10:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each { |food| cheese_types.include(food) }
+  array.each { |food| cheese_types.include?(food) }
   end
 
   
